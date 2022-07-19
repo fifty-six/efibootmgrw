34306d9db23e4eedd6413359a32b3f32bf89b615
fifty-six <ybham6@gmail.com>
2022-07-19 13:58:03 -0400

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -15,55 +15,55 @@
 namespace efibootmgrw {
 
 auto res_main(int argc, const char **argv_ptr) -> lak::result<lak::monostate> {
-        lak::span<const char *> argv { argv_ptr, static_cast<size_t>(argc) };
+    lak::span<const char *> argv { argv_ptr, static_cast<size_t>(argc) };
 
-        Context ctx;
+    Context ctx;
 
-        parse_args(ctx, argv);
+    parse_args(ctx, argv);
 
-        // alignas(8) lak::array<byte_t, 4096> buf;
-        // alignas(4) byte_t buf[4096];
-        caching_efi_load_option e;
+    // alignas(8) lak::array<byte_t, 4096> buf;
+    // alignas(4) byte_t buf[4096];
+    caching_efi_load_option e;
 
-        winapi::open_current_process_token(winapi::token_adjust_privileges | winapi::token_query)
-                .and_then(winapi::authenticate)
-                .and_then([&](auto) {
-                    return winapi::get_firmware_env_var(
-                            L"Boot0002",
-                            winapi::efi_global_variable,
-                            e.bytes()
-                    );
-                })
-                .if_ok([&](auto) {
-                    fmt::print(L"Boot0002: {}\n", e.desc(ctx).data());
-                    fmt::print("Writing to EFI Boot0001\n");
-                })
-                .and_then([&](lak::span<void> buf) {
-                    return winapi::set_firmware_env_var(
-                            L"Boot0001",
-                            winapi::efi_global_variable,
-                            buf
-                    );
-                })
-                // re-read it
-                .and_then([&](auto) {
-                    e.clear();
-                    return winapi::get_firmware_env_var(
-                            L"Boot0001",
-                            winapi::efi_global_variable,
-                            e.bytes()
-                    );
-                })
-                .if_ok([&](auto) {
-                    fmt::print(L"Boot0001: {}\n", e.desc(ctx).data());
-                })
-                .map_err(winapi::win_err::to_wstring)
-                .if_err([&](const lak::wstring &w) {
-                    Fatal(ctx, L"{}", w);
-                });
+    winapi::open_current_process_token(winapi::token_adjust_privileges | winapi::token_query)
+            .and_then(winapi::authenticate)
+            .and_then([&](auto) {
+                return winapi::get_firmware_env_var(
+                        L"Boot0002",
+                        winapi::efi_global_variable,
+                        e.bytes()
+                );
+            })
+            .if_ok([&](auto) {
+                fmt::print(L"Boot0002: {}\n", e.desc(ctx).data());
+                fmt::print("Writing to EFI Boot0001\n");
+            })
+            .and_then([&](lak::span<void> buf) {
+                return winapi::set_firmware_env_var(
+                        L"Boot0001",
+                        winapi::efi_global_variable,
+                        buf
+                );
+            })
+                    // re-read it
+            .and_then([&](auto) {
+                e.clear();
+                return winapi::get_firmware_env_var(
+                        L"Boot0001",
+                        winapi::efi_global_variable,
+                        e.bytes()
+                );
+            })
+            .if_ok([&](auto) {
+                fmt::print(L"Boot0001: {}\n", e.desc(ctx).data());
+            })
+            .map_err(winapi::win_err::to_wstring)
+            .if_err([&](const lak::wstring& w) {
+                Fatal(ctx, L"{}", w);
+            });
 
-        return lak::ok_t { };
-    }
+    return lak::ok_t { };
+}
 }
 
 int main(int argc, const char **argv) {
--- src/native_methods.h
+++ src/native_methods.h
@@ -8,119 +8,119 @@
 #include "WinBase.h"
 
 namespace efibootmgrw::winapi {
-    using dword = DWORD;
-    using luid_t = LUID;
+using dword = DWORD;
+using luid_t = LUID;
 
-    struct win_err {
-        dword err;
+struct win_err {
+    dword err;
 
-        explicit win_err(dword err) : err{ err } {}
+    explicit win_err(dword err) : err { err } {}
 
-        [[nodiscard]] auto wstring() const -> lak::wstring {
-            return lak::winapi::error_code_to_wstring(err);
-        }
-
-        [[nodiscard]] static auto to_wstring(const struct win_err &e) -> lak::wstring {
-            return e.wstring();
-        }
-    };
-
-    struct handle_t {
-        HANDLE handle;
-
-        explicit handle_t(HANDLE handle) : handle{ handle } {}
-
-        [[nodiscard]] auto data() -> HANDLE* {
-            return &handle;
-        }
-
-        [[nodiscard]] operator HANDLE() const { // NOLINT(google-explicit-constructor)
-            return handle;
-        }
-    };
-
-    struct tok_privs {
-        dword count;
-        luid_t luid;
-        dword attributes;
-    };
-
-    template<typename T>
-    using wresult = lak::result<T, win_err>;
-
-    const lak::wstring efi_global_variable = L"{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
-
-    const lak::wstring sys_env_priv = SE_SYSTEM_ENVIRONMENT_NAME;
-    const lak::wstring shutdown_priv = SE_SHUTDOWN_NAME;
-
-    constexpr dword token_adjust_privileges = TOKEN_ADJUST_PRIVILEGES;
-    constexpr dword token_query = TOKEN_QUERY;
-
-    [[nodiscard]]
-    inline auto get_last_error() -> win_err {
-        return win_err{ ::GetLastError() };
+    [[nodiscard]] auto wstring() const -> lak::wstring {
+        return lak::winapi::error_code_to_wstring(err);
     }
 
-    [[nodiscard]]
-    inline auto open_current_process_token(dword privs) -> wresult<handle_t> {
-        handle_t token{ nullptr };
+    [[nodiscard]] static auto to_wstring(const struct win_err& e) -> lak::wstring {
+        return e.wstring();
+    }
+};
 
-        auto proc = ::GetCurrentProcess();
+struct handle_t {
+    HANDLE handle;
 
-        if (!::OpenProcessToken(proc, privs, token.data()))
-            return lak::err_t{ get_last_error() };
+    explicit handle_t(HANDLE handle) : handle { handle } {}
 
-        return lak::ok_t{ token };;
+    [[nodiscard]] auto data() -> HANDLE * {
+        return &handle;
     }
 
-    [[nodiscard]]
-    inline auto open_current_process_token() -> wresult<handle_t> {
-        return lak::ok_t { handle_t { ::GetCurrentProcessToken() }};
+    [[nodiscard]] operator HANDLE() const { // NOLINT(google-explicit-constructor)
+        return handle;
+    }
+};
+
+struct tok_privs {
+    dword count;
+    luid_t luid;
+    dword attributes;
+};
+
+template<typename T>
+using wresult = lak::result<T, win_err>;
+
+const lak::wstring efi_global_variable = L"{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
+
+const lak::wstring sys_env_priv = SE_SYSTEM_ENVIRONMENT_NAME;
+const lak::wstring shutdown_priv = SE_SHUTDOWN_NAME;
+
+constexpr dword token_adjust_privileges = TOKEN_ADJUST_PRIVILEGES;
+constexpr dword token_query = TOKEN_QUERY;
+
+[[nodiscard]]
+inline auto get_last_error() -> win_err {
+    return win_err { ::GetLastError() };
+}
+
+[[nodiscard]]
+inline auto open_current_process_token(dword privs) -> wresult<handle_t> {
+    handle_t token { nullptr };
+
+    auto proc = ::GetCurrentProcess();
+
+    if (!::OpenProcessToken(proc, privs, token.data()))
+        return lak::err_t { get_last_error() };
+
+    return lak::ok_t { token };;
+}
+
+[[nodiscard]]
+inline auto open_current_process_token() -> wresult<handle_t> {
+    return lak::ok_t { handle_t { ::GetCurrentProcessToken() }};
+}
+
+[[nodiscard]]
+inline auto authenticate(handle_t token) -> wresult<lak::monostate> {
+    for (const lak::wstring& priv : { sys_env_priv, shutdown_priv }) {
+        tok_privs privs { };
+
+        fmt::print(L"looking up priv value for {}\n", priv);
+
+        if (!::LookupPrivilegeValueW(nullptr, priv.data(), &privs.luid))
+            return lak::err_t { get_last_error() };
+
+        fmt::print(L"adjusting token value for {}\n", priv);
+
+        privs.count += 1;
+        privs.attributes = SE_PRIVILEGE_ENABLED;
+
+        if (!::AdjustTokenPrivileges(token, false, reinterpret_cast<TOKEN_PRIVILEGES *>(&privs), 0, nullptr, nullptr))
+            return lak::err_t { get_last_error() };
+
+        fmt::print(L"adjusted for {}\n", priv);
     }
 
-    [[nodiscard]]
-    inline auto authenticate(handle_t token) -> wresult<lak::monostate> {
-        for (const lak::wstring& priv: { sys_env_priv, shutdown_priv }) {
-            tok_privs privs {};
+    return lak::ok_t { };
+}
 
-            fmt::print(L"looking up priv value for {}\n", priv);
+[[nodiscard]]
+inline auto get_firmware_env_var(lak::wstring_view name, lak::wstring_view guid, lak::span<void> buf)
+-> wresult<lak::span<void>> {
+    dword ret = ::GetFirmwareEnvironmentVariableW(name.data(), guid.data(), buf.data(), buf.size_bytes());
 
-            if (!::LookupPrivilegeValueW(nullptr, priv.data(), &privs.luid))
-                return lak::err_t { get_last_error() };
-
-            fmt::print(L"adjusting token value for {}\n", priv);
-
-            privs.count += 1;
-            privs.attributes = SE_PRIVILEGE_ENABLED;
-
-            if (!::AdjustTokenPrivileges(token, false, reinterpret_cast<TOKEN_PRIVILEGES*>(&privs), 0, nullptr, nullptr))
-                return lak::err_t { get_last_error() };
-
-            fmt::print(L"adjusted for {}\n", priv);
-        }
-
-        return lak::ok_t {};
+    if (ret == 0) {
+        return lak::err_t { get_last_error() };
     }
 
-    [[nodiscard]]
-    inline auto get_firmware_env_var(lak::wstring_view name, lak::wstring_view guid, lak::span<void> buf)
-    -> wresult<lak::span<void>> {
-        dword ret = ::GetFirmwareEnvironmentVariableW(name.data(), guid.data(), buf.data(), buf.size_bytes());
+    return lak::ok_t { lak::span<void> { lak::span<byte_t> { buf }.subspan(0, ret) }};
+}
 
-        if (ret == 0) {
-            return lak::err_t{ get_last_error() };
-        }
-
-        return lak::ok_t{ lak::span<void>{ lak::span<byte_t>{ buf }.subspan(0, ret) }};
+inline auto set_firmware_env_var(lak::wstring_view name, lak::wstring_view guid, lak::span<void> buf)
+-> wresult<lak::monostate> {
+    if (!::SetFirmwareEnvironmentVariableW(name.data(), guid.data(), buf.data(), buf.size_bytes())) {
+        return lak::err_t { get_last_error() };
     }
 
-    inline auto set_firmware_env_var(lak::wstring_view name, lak::wstring_view guid, lak::span<void> buf)
-    -> wresult<lak::monostate> {
-        if (!::SetFirmwareEnvironmentVariableW(name.data(), guid.data(), buf.data(), buf.size_bytes())) {
-            return lak::err_t{ get_last_error() };
-        }
-
-        return lak::ok_t{ };
-    }
+    return lak::ok_t { };
+}
 
 }
